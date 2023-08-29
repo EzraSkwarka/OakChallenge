b94babfc0c10bb1a122472ba796657f39d9e11f5
Ezra Skwarka <ezraskwarka@gmail.com>
2023-08-29 13:09:54 -0400

    New Form Structure

--- script.js
+++ script.js
@@ -1,5 +1,103 @@
 var debug = false;
 
+function sectionBuilder(sectionObject = []) {
+  var sectionDiv = document.createElement("div");
+  document.getElementById("sectionContainer").appendChild(sectionDiv);
+  // sectionDiv.innerHTML = sectionObject;
+  sectionDiv.id = sectionObject[0].toString();
+
+  for (index = 1; index < sectionObject.length; index++) {
+    var groupDiv = document.createElement("div");
+    groupDiv.id = sectionObject[0].toString() + "_" + index.toString();
+    document.getElementById(sectionDiv.id).appendChild(groupDiv);
+
+    for (groupIndex = 0; groupIndex < sectionObject[index].length; groupIndex++) {
+      var familyLineTextObject = "";
+      var checkBoxForm = '';
+      /*Since the family line can only be a max of three mons, instead of making another for loop I'm just gonna define the three cases */
+      var familyLineArray = sectionObject[index][groupIndex];
+      var familyLineDiv = document.createElement("div");
+      groupDiv.appendChild(familyLineDiv);
+      switch (familyLineArray.length) {
+        case 5:
+          //Create Check box form
+          var checkBoxForm = document.createElement("form");
+          checkBoxForm.id = familyLineArray[4].toString() + "_form";
+          familyLineDiv.appendChild(checkBoxForm);
+          //Create the input
+          var checkBoxInput_5 = document.createElement("input");
+          checkBoxInput_5.id = familyLineArray[4].toString() + "_input";
+          checkBoxInput_5.type = "checkbox";
+          checkBoxForm.appendChild(checkBoxInput_5);
+          //Create the label
+          var checkBoxLabel_5 = document.createElement("label");
+          checkBoxLabel_5.htmlFor = checkBoxInput_5.id.toString();
+          checkBoxLabel_5.innerText =
+            " -> by " +
+            familyLineArray[3][0].toString() +
+            " @ " +
+            familyLineArray[3][1].toString() +
+            " " +
+            familyLineArray[4].toString();
+          checkBoxForm.appendChild(checkBoxLabel_5);
+
+        // familyLineTextObject =
+        // checkBoxLabel_5.innerText;
+
+        case 3:
+          //See if for exists, otherwise make one
+          
+          if (checkBoxForm == '') {
+            var checkBoxForm = document.createElement("form");
+            checkBoxForm.id = familyLineArray[2].toString() + "_form";
+            familyLineDiv.appendChild(checkBoxForm);
+            
+          }
+          //Create the input
+          var checkBoxInput_3 = document.createElement("input");
+          checkBoxInput_3.id = familyLineArray[2].toString() + "_input";
+          checkBoxInput_3.type = "checkbox";
+          checkBoxForm.insertBefore(checkBoxInput_3, checkBoxForm.firstChild);
+
+          //Create the label
+          var checkBoxLabel_3 = document.createElement("label");
+          checkBoxLabel_3.htmlFor = checkBoxInput_3.id.toString();
+          checkBoxLabel_3.innerText =
+            " -> by " +
+            familyLineArray[1][0].toString() +
+            " @ " +
+            familyLineArray[1][1].toString() +
+            " " +
+            familyLineArray[2].toString() +
+            familyLineTextObject;
+
+          checkBoxInput_3.after(checkBoxLabel_3);
+
+        default:
+          //See if for exists, otherwise make one
+          if (checkBoxForm == '') {
+            var checkBoxForm = document.createElement("form");
+            checkBoxForm.id = familyLineArray[0].toString() + "_form";
+            familyLineDiv.appendChild(checkBoxForm);
+          }
+
+          //Create the input
+          var checkBoxInput = document.createElement("input");
+          checkBoxInput.id = familyLineArray[0].toString() + "_input";
+          checkBoxInput.type = "checkbox";
+          checkBoxForm.insertBefore(checkBoxInput, checkBoxForm.firstChild);
+
+          //Create the label
+          var checkBoxLabel = document.createElement("label");
+          checkBoxLabel.htmlFor = checkBoxInput.id.toString();
+          checkBoxLabel.innerText = familyLineArray[0].toString();
+          checkBoxInput.after(checkBoxLabel)
+      }
+    }
+    groupDiv.innerHTML += "<br>";
+  }
+}
+
 function badgeSection(discriminator, textArray) {
   createBadgeContainer(discriminator);
 
