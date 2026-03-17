79258c6497c48bf2c5bf005210b2b1fea9ca4240
Ezra Skwarka <ezraskwarka@gmail.com>
2026-03-17 16:01:54 -0400

    basic writeup and image dump

---
+++ data/minimal_template.js
@@ -0,0 +1,27 @@
+/* ----------------------
+ - img src: 
+ - Guide: 
+----------------------- */
+window.gameData = {
+  gameId: ``,
+  gameTitle: ``,
+
+  logo: ``,
+
+  aboutHtml: ``,
+
+  progression: {
+    "Section Title": {
+      headerTitle: ``,
+      headerImg: ``,
+      headerImgAlt: ``,
+
+      summaryShort: ``,
+
+      summaryHtml: ``,
+      summaryOpen: false,
+
+      rows: []
+    }
+  }
+}
